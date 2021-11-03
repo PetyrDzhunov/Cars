aef65b9ea0466b9a477bd5c379e7244dadf247b5
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-03 18:45:57 +0200

    connected my cloud mongoDB

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -3,4 +3,4 @@ const { DB_CONNECTION_STRING } = require('../constants');
 
 exports.initDatabase = function() {
     return mongoose.connect(DB_CONNECTION_STRING);
-}
+};
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 exports.PORT = 4000;
-exports.DB_CONNECTION_STRING = "mongodb://localhost:27017/cars-shop";
+exports.DB_CONNECTION_STRING = "mongodb+srv://PetyrDzhunov:pecata941102@cubicles.hdzkr.mongodb.net/Cars-shop?retryWrites=true&w=majority"
