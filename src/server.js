ab3031f6af9580e3e040a137ce0b0f6010bbfe59
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-03 15:57:06 +0200

    express-configuration

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { PORT } = require('./constants');
 
 const app = express();
+require('./config/express-config')(app);
 
 app.get('/', (req, res) => {
     res.send('Its working');
 });
 
-app.listen(PORT, () => console.log(`Server is now running at http://localhost/${PORT}`))
+app.listen(PORT, () => console.log(`Server is now running at http://localhost:${PORT}/`))
