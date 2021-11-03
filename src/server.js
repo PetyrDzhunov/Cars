c837a878af632075a4d449b251561f21011247d4
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-03 17:48:20 +0200

    created handlebars-configuration and changed its default views lookup

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const { PORT } = require('./constants');
 
 const app = express();
+require('./config/handlebars-config')(app);
 require('./config/express-config')(app);
 
 app.get('/', (req, res) => {
-    res.send('Its working');
+    res.render('home');
 });
 
 app.listen(PORT, () => console.log(`Server is now running at http://localhost:${PORT}/`))
