3ab4e24858d7be16ad669db81ce5b9805da13cd9
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-07 23:27:55 +0200

    fixed some CSS and created search page

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -6,4 +6,9 @@ router.get('/', async(req, res) => {
     res.render('home', { title: 'Car market homepage', cars })
 });
 
+router.get('/search', (req, res) => {
+    console.log(req.query.text);
+    res.render('search', { title: 'Search Cars' });
+});
+
 module.exports = router;
