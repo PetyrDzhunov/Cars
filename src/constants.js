4cbd1e54687ecdf7178a6a39c2c3a359a8f4c903
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-11-09 11:21:15 +0200

    making some changes so I can deploy to heroku

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-exports.PORT = 4000;
+exports.PORT = process.env.PORT || 4000;
 exports.DB_CONNECTION_STRING = "mongodb+srv://PetyrDjunov:secretPassword@cubicles.hdzkr.mongodb.net/Cars-shop?retryWrites=true&w=majority";
 exports.SALT_ROUNDS = 10;
 exports.AUTH_COOKIE_NAME = 'token';
